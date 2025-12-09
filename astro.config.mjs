7f709588d40c331f53bc1b28520eb942a261fc52
jepeez <jerry.jussila@gmail.com>
2025-12-09 22:38:42 +0200

    Add conditional base path for GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jepeez.github.io',
+    base: process.env.GITHUB_ACTIONS ? '/aihio' : '/',
     integrations: [react(), tailwind()],
     compressHTML: true,
     prefetch: true,
