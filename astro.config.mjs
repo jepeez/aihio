0d00f569020b0f3437e585970742d7e7d605590b
jepeez <jerry.jussila@gmail.com>
2025-12-09 22:46:17 +0200

    Use NODE_ENV production check for base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jepeez.github.io',
-    base: process.env.GITHUB_BASE_PATH || '/',
+    base: process.env.NODE_ENV === 'production' ? '/aihio' : '/',
     integrations: [react(), tailwind()],
     compressHTML: true,
     prefetch: true,
