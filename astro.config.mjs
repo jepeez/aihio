c7681df85cb02aaaf191d49798d3d5c9ee6208c1
jepeez <jerry.jussila@gmail.com>
2025-12-09 22:23:12 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jepeez.github.io',
+    base: '/aihio',
     integrations: [react(), tailwind()],
     compressHTML: true,
     prefetch: true,
